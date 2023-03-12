e38d3976e8be390511c2f228f3fdb7517a9a8896
DmitriyKras <dmitriy_krasnov@outlook.com>
2023-03-12 13:53:47 +0300

    src folder with prepare.py added

---
+++ src/prepare.py
@@ -0,0 +1,38 @@
+import json
+import os
+import sys
+from sklearn.model_selection import train_test_split
+
+os.chdir("../")  # move back to project's root
+
+with open("prepare_config.json", "r") as f:  # load config file
+    config = json.load(f)
+    
+root = os.getcwd()  # get path to root
+utils_path = os.path.join(root, "utils/")  # get path to utils
+
+sys.path.append(utils_path)  # append to sys path
+
+from utils import get_img_mask_pairs  # import function from utils
+
+img_folder = os.path.join(root, config["images_folder"])  # get pathes to img
+mask_folder = os.path.join(root, config["masks_folder"])  # and masks
+# get train and val pairs
+train_pairs, val_pairs = get_img_mask_pairs(img_folder,
+                                            mask_folder, 
+                                            val_size=0.2)
+# get train and test pairs
+train_pairs, test_pairs = train_test_split(train_pairs, test_size=0.1)
+# get destination pathes for train, val and test
+train_path = os.path.join(root, config["train_pairs_path"])
+val_path = os.path.join(root, config["val_pairs_path"])
+test_path = os.path.join(root, config["test_pairs_path"])
+# write json files
+with open(train_path, "w") as f:
+    f.write(json.dumps(train_pairs))
+with open(val_path, "w") as f:
+    f.write(json.dumps(val_pairs))
+with open(test_path, "w") as f:
+    f.write(json.dumps(test_pairs))
+
+print("Data preparing completed")
